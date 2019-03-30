27ab240a472a0ecb3bcbecc4b2452a61f60b6553
Maison Galvão <maisongalvao144@gmail.com>
2019-03-30 16:13:42 -0400

    Aula 02 - Guardando qualquer referencia

--- src/br/com/bytebank/banco/test/TesteArrayReferencia.java
+++ src/br/com/bytebank/banco/test/TesteArrayReferencia.java
@@ -1,7 +1,21 @@
 package br.com.bytebank.banco.test;
 
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
+
 public class TesteArrayReferencia {
     public static void main(String[] args) {
 
+        Conta[] contas = new Conta[5];
+        ContaCorrente cc1 = new ContaCorrente(124,12563);
+        ContaPoupanca cp1 = new ContaPoupanca(124,12563);
+        contas[0]=cc1;
+        contas[1]=cp1;
+
+        System.out.println(cp1.getNumero());
+        System.out.println(cc1.getNumero());
+        System.out.println(contas[1].getNumero());
+        System.out.println(contas[1]==cp1 ? true: false);
     }
 }
