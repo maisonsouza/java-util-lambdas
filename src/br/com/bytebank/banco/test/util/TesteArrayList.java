b2e5426ade5808a21e2e43553db961e1099ab90e
Maison Galvão <maisongalvao144@gmail.com>
2019-04-01 14:13:47 -0400

    Aula 05 - Vector e a interface Collection

--- src/br/com/bytebank/banco/test/util/TesteArrayList.java
+++ src/br/com/bytebank/banco/test/util/TesteArrayList.java
@@ -5,12 +5,14 @@ import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Vector;
 
 public class TesteArrayList {
 
     public static void main(String[] args) {
-        List<Conta> lista = new ArrayList<Conta>();
+        Collection<Conta> lista = new Vector<Conta>();
         Conta cc1 = new ContaCorrente(121, 123);
         Conta cp1 = new ContaPoupanca(122, 123);
         lista.add(cc1);
